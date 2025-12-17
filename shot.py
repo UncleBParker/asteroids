234e6eac9b7d00e31c55131b8df37dec042a5446
UncleBParker <bparker091924@gmail.com>
2025-12-16 20:21:20 -0600

    Add game source files

---
+++ shot.py
@@ -0,0 +1,15 @@
+import pygame
+from circleshape import CircleShape
+from constants import LINE_WIDTH, SHOT_RADIUS
+
+class Shot(CircleShape):
+	def __init__(self, x, y, radius=SHOT_RADIUS):
+		#print("creating shot")
+		super().__init__(x, y, radius)
+
+	def draw(self, screen):
+		#print("drawing shot at", self.position)
+		pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
+
+	def update(self, dt):
+		self.position += self.velocity * dt
