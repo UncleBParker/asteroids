26d74a650e2b4f74c04dbc0728cee347e2ceae50
UncleBParker <bparker091924@gmail.com>
2025-12-17 20:02:00 -0600

    added player-shot/asteroid collisions & asteroid splitting on collisions

--- main.py
+++ main.py
@@ -42,6 +42,11 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
         #print(f"drawable size: {len(drawable)}")
         for each in drawable:
             each.draw(screen)
